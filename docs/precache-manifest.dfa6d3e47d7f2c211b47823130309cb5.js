694bc326926d0d1020fc412f90ffc083e7993310
pengliang.zhu <pengliang.zhu@clouddeep.cn>
2019-12-05 10:43:44 +0800

    支持git部署

---
+++ docs/precache-manifest.dfa6d3e47d7f2c211b47823130309cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7ac088eae750f7d6a84c01b7af272a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74b30bf06b48b3eab34a",
+    "url": "/static/css/2.b9c1f6b4.chunk.css"
+  },
+  {
+    "revision": "2444de9fdcd7eb687351",
+    "url": "/static/css/main.6049d8bf.chunk.css"
+  },
+  {
+    "revision": "74b30bf06b48b3eab34a",
+    "url": "/static/js/2.4f9a7b9e.chunk.js"
+  },
+  {
+    "revision": "2444de9fdcd7eb687351",
+    "url": "/static/js/main.81d9b1f6.chunk.js"
+  },
+  {
+    "revision": "f5128e168581597993f6",
+    "url": "/static/js/runtime-main.89333823.js"
+  }
+]);
